75190e7d89b8700cd0fb0340e98ac646080b6e3b
dylwon <122586486+dylwon@users.noreply.github.com>
2024-01-27 16:13:11 -0500

    StylophoneProject.c

--- StylophoneProject.c
+++ StylophoneProject.c
@@ -1,82 +1,95 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#define Pad PA1
+#define PitchControl PA2
+#define VolumeControl PA3
+#define Buzzer PA6
+#define LED PA7
+#define MAX_VOLT 1023
 
-void PWM_init() {
+// Frequency values for each note
+#define NOTE_C4 262
+#define NOTE_Db4 277
+#define NOTE_D4 294
+#define NOTE_Eb4 311
+#define NOTE_E4 330
+#define NOTE_F4 349
+#define NOTE_Gb4 370
+#define NOTE_G4 392
+#define NOTE_Ab4 415
+#define NOTE_A4 440
+#define NOTE_Bb4 466
+#define NOTE_B4 494
+#define NOTE_C5 523
+
+// Voltage values for each note
+#define VOLT_C4 0
+#define VOLT_Db4 1
+#define VOLT_D4 2
+#define VOLT_Eb4 3
+#define VOLT_E4 4
+#define VOLT_F4 5
+#define VOLT_Gb4 6
+#define VOLT_G4 7
+#define VOLT_Ab4 8
+#define VOLT_A4 9
+#define VOLT_Bb4 10
+#define VOLT_B4 11
+#define VOLT_C5 12
+
+unsigned int Frequency(int voltage, unsigned int volt_scale[13], unsigned int frequency_scale[13], int pitchShift) {
   
-    // [Configure Timer/Counter1 for PWM (Fast PWM mode)]
-    // (Fast PWM means it hard resets the counter after comparison)
-    
-    // COM1B1 configures the timer to be reset after comparison
-    // PWM1B ???
-    TCCR1A |= (1 << COM1B1) | (1 << PWM1B); 
-    
-    // WGM11 & WGM10 sets the mode of operation to be fast PWM 
-    // CS10 sets Prescaler = 8 [20MHz System Clk/8+1] = 2.222MHz PWM Frequency
-    TCCR1B |= (1 << CS10); // Sets prescaler to 8
-    TCCR1D |= (1 << WGM11) | (1 << WGM10); // Set Fast PWM mode with OCR1A as the TOP value
+  float lowestError = 500.0, error;
+  int index = 0;
 
-    // Initializes the PWM duty cycle to 50% (0 to 255)
-    //OCR1A is the TOP value for the timer
-    OCR1A = 128;
-}
-
-void PWM_out(uint16_t pwm_frequency, uint16_t volume) {
-  
-    // [Modify Frequency for PWM]
-
-    // Calculate the required prescaler value
-    uint16_t prescaler_value;
-    if (pwm_frequency <= 0) {
-        prescaler_value = 0;  // No prescaling
-    } 
-    else {
-        // Choose a prescaler value that gives a frequency less than or equal to the desired frequency
-        prescaler_value = (F_CPU / (2 * pwm_frequency) - 1);
-        if (prescaler_value > 16383) {
-            prescaler_value = 16383;  // Maximum prescaler value
-        }
+  // For-loop calculates which voltage value in the volt_scale is the closest to the measured voltage value using percent error.
+  // Error is calculated by taking the float absolute value of of the ((current pad's voltage - measured voltage) / current pad's voltage)) * 100. Values were type casted to ensure error remains a "double" type value.
+  for (int i = 0; i < sizeof(volt_scale)/sizeof(unsigned int); i++) {
+    error = fabs((volt_scale[i] - voltage)/float(volt_scale[i])) * 100.0;
+    if (error < lowestError) { // If the current pad's voltage is the closest to the measured voltage, then we save the error value and index.
+      lowestError = error;
+      index = i;
     }
-
-    // Configure Timer/Counter1 for PWM (Fast PWM mode)
-    TCCR1A |= (1 << COM1B1) | (1 << PWM1B); // Set non-inverting mode and Fast PWM mode
-    TCCR1B |= ((prescaler_value & 0x07) << CS10); // Set prescaler
-    TCCR1D |= (1 << WGM11) | (1 << WGM10); // Set Fast PWM mode with OCR1A as the TOP value
-}
-
-void PWMout(unsigned char dutyCycle, float freq) {
-  TCCR1A = 0x21; // 0b 0010 0001
-  TCCR1B = 0x14; // 0b 0001 0100
-  OCR1A = 0x7A12 / freq;
-  OCR1B = OCR1A * (dutyCycle / 255.0);
+  }
+  
+  // Returns the frequency given by the detected pad +- a percentage of itself.
+  // The percentage is defined by calculating the decimal value of the (pitchShift analog reading/MAX_VOLT), and is then multiplied with the current frequency to return an integer value.
+  // To ensure that there's no overflow when converting a negative integer value to an unsigned value, we used this if-statement that returns 0 if the integer is negative, and returns the integer value if it's positive.
+  if ((frequency_scale[index] + ((pitchShift/(float)MAX_VOLT) * frequency_scale[index])) <= 0.0)
+    return 0; 
+  else 
+    return frequency_scale[index] + ((pitchShift/(float)MAX_VOLT) * frequency_scale[index]);
 }
 
 void setup() {
-
-  // [Initialization of Pin Directions]
+  Serial.begin(9600); // Set baud rate for debugging
   
-  // Sets PA0 as an input
-  // PA1 reads the digital voltage value from the resistor ladder
-  DDRA |= (0 << PA0);
+  // [Initializing Pins as Inputs/Outputs]
   
-  // Sets PA6 and PA7 as outputs
-  // PA6 is the PWM output that feeds into a buzzer
-  // PA7 is an indication LED that lights up when the MCU is powered
-  DDRA |= (1 << PA6) | (1 << PA7);
-
-  // Sets PB0-PB2 as inputs
-  // PA0 is the designated MOSI pin (Master-Out-Slave-In)
-  // PA1 is the designated MISO pin (Master-In-Slave-Out)
-  // PA2 is the designated SCK pin (Serial Data Clock)
-  // DDRB |= (0 << PB0) | (0 << PB1) | (0 << PB2);
-
-  // [Initialization of Pin Values]
+  // PA1 is the analog input from the resistor ladder
+  // PA2 is the analog input from the Pitch Control potentiometer
+  // PA3 is the analog input from the Volume Control potentiometer
+  DDRA &= (~PIN1 | ~PIN2 | ~PIN3);
   
-  // Sets PA7 output to low to turn the indication LED on
-  PORTA |= (0 << PA7); 
+  // PA6 is the PWM output for the buzzer
+  // PA7 is the output to power the indication LED 
+  DDRA |= (PIN6 | PIN7);  
+
+  // [Setting the Input/Output Values]
+
+  // Sets the bit at PA7 to be 0 to turn the LED on.
+  PORTA &= (~PIN7);
 }
 
 void loop() {
-  //Read the digital value of PA0
-  //Determine what frequency we need to output to the buzzer using PWM
-  //Write a function that modifies the PWM frequency to match the necessary value
+  unsigned int frequency_scale[] = {NOTE_C4, NOTE_Db4, NOTE_D4, NOTE_Eb4, NOTE_E4, NOTE_F4, NOTE_Gb4, NOTE_G4, NOTE_Ab4, NOTE_A4, NOTE_Bb4, NOTE_B4, NOTE_C5};
+  unsigned int volt_scale[] = {VOLT_C4, VOLT_Db4, VOLT_D4, VOLT_Eb4, VOLT_E4, VOLT_F4, VOLT_Gb4, VOLT_G4, VOLT_Ab4, VOLT_A4, VOLT_Bb4, VOLT_B4, VOLT_C5};
+  int voltage;
+  int pitchShift;                                                    
+  while (1) {
+    voltage = analogRead(Pad); // Analog voltage reading at PA1. This value is used to determine which pad was pressed.
+    pitchShift = analogRead(PitchControl); // Analog voltage reading at PA2. We use this value to increase/decrease the frequency linearly based on the chosen frequency.
+    pitchShift -= MAX_VOLT/2; // We subtract by MAX_VOLT/2 because we want values between 0-516 (0V-2.5V) to lower the pitch, while values of between 517-1023 will raise the pitch (2.5V-5.0V). This is because the potentiometer will be physically centered at 2.5V
+    tone(Buzzer, Frequency(analogRead(Pad), volt_scale, frequency_scale, pitchShift));
+    noTone();
+  }
 }
